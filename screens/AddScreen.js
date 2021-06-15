9b8fd8885145d10b7a8b6a6b5d54f432ac909afd
SimNee <chua_sim_nee@hotmail.com>
2021-06-15 23:08:27 +0800

    Added add note function

--- screens/AddScreen.js
+++ screens/AddScreen.js
@@ -1,29 +1,66 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
   TouchableOpacity,
   StyleSheet,
   TextInput,
+  Button
 } from "react-native";
+
+import { API, graphqlOperation } from 'aws-amplify'
+import { createTodo } from '../src/graphql/mutations'
+import { listTodos } from '../src/graphql/queries'
+
+const initialState = { name: '', description: '' }
 
 export default function AddScreen({ navigation }) {
   const [text, setText] = useState("");
+
+  const [formState, setFormState] = useState(initialState)
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    fetchTodos()
+  }, [])
+
+  function setInput(key, value) {
+    setFormState({ ...formState, [key]: value })
+  }
+
+  async function fetchTodos() {
+    try {
+      const todoData = await API.graphql(graphqlOperation(listTodos))
+      const todos = todoData.data.listTodos.items
+      setTodos(todos)
+    } catch (err) { console.log('error fetching todos') }
+  }
+
+  async function addTodo() {
+    try {
+      const todo = { ...formState }
+      setTodos([...todos, todo])
+      setFormState(initialState)
+      await API.graphql(graphqlOperation(createTodo, {input: todo}))
+    } catch (err) {
+      console.log('error creating todo:', err)
+    }
+  }
 
   return (
     <View style={[styles.container, { backgroundColor: "white" }]}>
       <Text style={{ fontSize: 24 }}>What do you want to add?</Text>
       <TextInput
+        onChangeText={val => setInput('name', val)}
         style={styles.textInput}
-        value={text}
-        onChangeText={(input) => setText(input)}
+        value={formState.name}
       />
       <View style={styles.buttonContainer}>
         <TouchableOpacity
           style={styles.button}
           onPress={() => navigation.navigate("Notes", { text })}
         >
-          <Text style={styles.buttonText}>Save</Text>
+          <Button style={styles.buttonText} title="Save" onPress={addTodo} />
         </TouchableOpacity>
         <TouchableOpacity
           style={styles.button}
