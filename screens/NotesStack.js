7c6c270a7c7f6c97fed24fb7059f3d00f80e1457
Chow Jia Ying <chowjiaying211@gmail.com>
2021-06-15 23:30:10 +0800

    delete login screen from app

--- screens/NotesStack.js
+++ screens/NotesStack.js
@@ -1,12 +1,11 @@
 import * as React from "react";
-import { Text, View, Button } from "react-native";
-import { createStackNavigator } from "@react-navigation/stack";
-import LoginScreen from "./LoginScreen";
+import {Text, View, Button} from "react-native";
+import {createStackNavigator} from "@react-navigation/stack";
 import AddScreen from "./AddScreen";
 import NotesScreen from "./NotesScreen";
 
 const InnerStack = createStackNavigator();
-function NotesStack({ navigation }) {
+function NotesStack({navigation}) {
   return (
     <InnerStack.Navigator>
       <InnerStack.Screen
@@ -24,7 +23,6 @@ export default function StackNavigator() {
     <Stack.Navigator >
       <Stack.Screen name="Notes Stack" component={NotesStack} />
       <Stack.Screen name="Add Screen" component={AddScreen} />
-      <Stack.Screen name="Login" component={LoginScreen} />
     </Stack.Navigator>
   );
 }
