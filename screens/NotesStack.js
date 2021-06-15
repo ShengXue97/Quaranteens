5d19e66c2089a31740760a6752315afb00a2bea3
ShengXue97 <simshengxue1997@gmail.com>
2021-06-15 22:47:08 +0800

    refactor NotesScreen

--- screens/NotesStack.js
+++ screens/NotesStack.js
@@ -12,21 +12,6 @@ function NotesStack({ navigation }) {
       <InnerStack.Screen
         name="Notes"
         component={NotesScreen}
-        options={{
-          title: "Diary1",
-          headerStyle: {
-            backgroundColor: "yellow",
-            height: 100,
-            shadowColor: "black",
-            shadowOpacity: 0.2,
-            shadowRadius: 5,
-          },
-          headerTintColor: "#f55",
-          headerTitleStyle: {
-            fontSize: 24,
-            fontWeight: "bold",
-          },
-        }}
       />
     </InnerStack.Navigator>
   );
