2af609a89c4f098e7865445c4b009a292b16e5f9
Chow Jia Ying <chowjiaying211@gmail.com>
2021-06-16 20:11:48 +0800

    change text in view entry

--- screens/ViewEntry.js
+++ screens/ViewEntry.js
@@ -9,16 +9,18 @@ export default function ViewEntry({route}) {
   const {item} = route.params;
   return (
     <View style={styles.container}>
-      <View styles={styles.flatList}>
-      <Text
-        style={styles.diaryTitle}
-        numberOfLines={1}
-      >
-        {item.name}
-      </Text>
-      <Text numberOfLines={20}>
-        {item.description}
-      </Text>
+      <View style={styles.flatList}>
+        <Text
+          style={styles.diaryTitle}
+          numberOfLines={1}
+        >
+          {item.name}
+        </Text>
+          <Text
+            style={{fontFamily: 'Campuri Book'}}
+            numberOfLines={20}>
+          {item.description}
+        </Text>
       </View>
     </View>
   );
@@ -27,15 +29,18 @@ export default function ViewEntry({route}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffc',
+    backgroundColor: '#a8a9b0',
     padding: 10,
   },
   flatList: {
-    padding: 10,
+    padding: 20,
+    height: '100%',
+    backgroundColor: '#fff1eb',
     borderColor: '#ccc',
     borderWidth: 1,
     borderRadius: 5,
-    marginTop: 10,
+    marginTop: 20,
+    marginBottom: 20,
   },
   diaryTitle: {
     fontSize: 20,
